212567a506eeb4e04482c8213be8a2e0e65b0edf
brettabamonte <abamonteb@gmail.com>
2024-03-31 13:32:39 -0400

    R script that uses boxplots for each continous variable to identify outliers

---
+++ IdentifyOutliers.R
@@ -0,0 +1,22 @@
+install.packages('tidyverse');
+install.packages("GGally")
+
+library(GGally)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+# Read the data from a CSV file
+dataset <- read.csv('Student Spending - Dataset.csv')
+
+dataset_longer <- pivot_longer(dataset, cols = c(monthly_income, financial_aid, tuition, housing, food, transportation, books_supplies, entertainment, personal_care, technology, health_wellness, miscellaneous), names_to = "Variable", values_to = "Value")
+
+# Create boxplots
+boxplots <- ggplot(dataset_longer, aes(x = Variable, y = Value)) +
+  geom_boxplot() +
+  facet_wrap(~ Variable, scales = "free") + # Use 'scales = "free_y"' if you want separate y-axis scales
+  theme_minimal()
+
+# Display the plot
+print(boxplots)
+
