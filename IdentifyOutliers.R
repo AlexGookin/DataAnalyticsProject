e65269db805ac4cf246f8d28907362b3a540f617
brettabamonte <abamonteb@gmail.com>
2024-03-31 14:32:40 -0400

    Add normalized and non normalized data frames. Use these data frames to display boxplots

--- IdentifyOutliers.R
+++ IdentifyOutliers.R
@@ -1,22 +1,36 @@
 install.packages('tidyverse');
-install.packages("GGally")
+install.packages("GGally");
 
 library(GGally)
 library(ggplot2)
-library(dplyr)
 library(tidyr)
+library(gridExtra)
+library(grid)
 
 # Read the data from a CSV file
 dataset <- read.csv('student-spending-dataset-cleaned.csv')
 
-dataset_longer <- pivot_longer(dataset, cols = c(monthly_income, financial_aid, tuition, housing, food, transportation, books_supplies, entertainment, personal_care, technology, health_wellness, miscellaneous), names_to = "Variable", values_to = "Value")
+#normalized data. Uses z-scoring
+dataset_normalized <- as.data.frame(scale(select_if(dataset, is.numeric)))
+
+#Convert dataframes into long format
+dataset_long <- pivot_longer(dataset, cols = c(monthly_income, financial_aid, tuition, housing, food, transportation, books_supplies, entertainment, personal_care, technology, health_wellness, miscellaneous), names_to = "Variable", values_to = "Value")
+normalized_long <- pivot_longer(dataset_normalized, c(monthly_income, financial_aid, tuition, housing, food, transportation, books_supplies, entertainment, personal_care, technology, health_wellness, miscellaneous), names_to = "Variable", values_to = "Value")
 
 # Create boxplots
-boxplots <- ggplot(dataset_longer, aes(x = Variable, y = Value)) +
+unnormalized_boxplots <- ggplot(dataset_long, aes(x = Variable, y = Value)) +
   geom_boxplot() +
   facet_wrap(~ Variable, scales = "free") + # Use 'scales = "free_y"' if you want separate y-axis scales
-  theme_minimal()
+  theme(plot.title = element_text(hjust=0.5)) +
+  ggtitle("Not Normalized")
 
-# Display the plot
-print(boxplots)
+normalized_boxplots <- ggplot(normalized_long, aes(x = Variable, y = Value)) +
+  geom_boxplot() +
+  facet_wrap(~ Variable, scales = "free") + # Use 'scales = "free_y"' if you want separate y-axis scales
+  theme(plot.title = element_text(hjust=0.5)) +
+  ggtitle("Normalized")
+
+
+# Display the plots
+grid.arrange(normalized_boxplots, unnormalized_boxplots, ncol = 2)
 
